e72e8608cc5718f3618b48da5b8b4b5e94d4f37d
Kris Szafranski <kris@thoughtshelter.com>
2020-05-15 13:13:13 -0500

    fixed ampersand

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Pets & Owners</h1>
+        <h1>Pets &amp; Owners</h1>
         <AddPetForm />
         <PetList />
       </div>
